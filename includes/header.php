641b4acca6a6a0237ae616949d9fa14e70999cab
Abid Nazir <abid.nazir@spsnet.com>
2025-12-20 04:05:22 +0500

    changes

--- includes/header.php
+++ includes/header.php
@@ -15,19 +15,10 @@ include("config.php");
     <meta name="keywords" content="TechBiz - IT Solution & Service HTML Template">
     <meta name="robots" content="INDEX,FOLLOW">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= $app_path ?>assets/img/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= $app_path ?>assets/img/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= $app_path ?>assets/img/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= $app_path ?>assets/img/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= $app_path ?>assets/img/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= $app_path ?>assets/img/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= $app_path ?>assets/img/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= $app_path ?>assets/img/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= $app_path ?>assets/img/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= $app_path ?>assets/img/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= $app_path ?>assets/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= $app_path ?>assets/img/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= $app_path ?>assets/img/favicons/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= $app_path ?>assets/img/easyplus/favicon/apple.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= $app_path ?>assets/img/easyplus/favicon/96size.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= $app_path ?>assets/img/easyplus/favicon/192size.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="<?= $app_path ?>assets/img/easyplus/favicon/512size.png">
     <link rel="manifest" href="<?= $app_path ?>assets/img/favicons/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="<?= $app_path ?>assets/img/favicons/ms-icon-144x144.png">
