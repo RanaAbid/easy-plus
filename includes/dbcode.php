2e7ae3236e465735a5624aad4dc8f6600d457689
Usama <usama@spsnet.com>
2026-01-02 15:19:37 +0500

    changes in dbcode

--- includes/dbcode.php
+++ includes/dbcode.php
@@ -4,7 +4,10 @@ $hostname="localhost";
 $mysql_login="root";
 $mysql_password="";
 $database="easy-plus";   
-
+// $hostname="localhost";
+// $mysql_login="tatours_easyplus";
+// $mysql_password="oF]QRf#{)8EU%CEH";
+// $database="tatours_easypluss";   
 
 $link = mysqli_connect($hostname,$mysql_login,$mysql_password,$database);
 // Check connection
